1016f513bd223d2a7fccbfa9ce43096ad5f4b938
Amir <Amirrezz@stud.ntnu.no>
2021-04-22 16:38:55 +0200

    Plots Updated Time axis Updated Plots can be saved

--- OCP/Battery_OCP.jl
+++ OCP/Battery_OCP.jl
@@ -11,13 +11,13 @@ N_hour = 6;
 Πᶠ            = NaN*ones(N_hour)
 Πᵉ            = NaN*ones(N_hour)
 
-Πᶠ[1:convert(Int64, N_hour/3)]     .=  6
-Πᶠ[convert(Int64, N_hour/3) + 1:2*convert(Int64, N_hour/3)]  .= 5
-Πᶠ[2*convert(Int64, N_hour/3) + 1:end]  .= 5
+Πᶠ[1:convert(Int64, N_hour/3)]     .=  1.5
+Πᶠ[convert(Int64, N_hour/3) + 1:2*convert(Int64, N_hour/3)]  .= 4
+Πᶠ[2*convert(Int64, N_hour/3) + 1:end]  .= 2.5
 
-Πᵉ[1:convert(Int64, N_hour/3)]     .=  8
-Πᵉ[convert(Int64, N_hour/3) + 1:2*convert(Int64, N_hour/3)]  .= 5.0
-Πᵉ[2*convert(Int64, N_hour/3) + 1:end]  .= 3
+Πᵉ[1:convert(Int64, N_hour/3)]     .=  5
+Πᵉ[convert(Int64, N_hour/3) + 1:2*convert(Int64, N_hour/3)]  .= 1
+Πᵉ[2*convert(Int64, N_hour/3) + 1:end]  .= 2.5
 
 # Πᵉ[1:convert(Int64, N_hour)]     .=  5
 
@@ -27,13 +27,14 @@ F0 = 0;
 O0 = 0;
 l0 = 0;
 P0 = 0;
-E0 = 4.0;
+E0 = 3.0;
 
 #ISO data
-ISO_sec = 1;
+ISO_sec = 10;
 N_ISO = convert(Int64, 3600/ISO_sec)
 # α = rand(N_hour , N_ISO)
 α =  -1*ones(N_hour , N_ISO)
+
 dt = ISO_sec
 
 
@@ -53,7 +54,7 @@ Eₘₐₓ = 10;
 m1 = Model(Ipopt.Optimizer)
 
 
-#Variable Defining----------------------------------
+##Variable Defining----------------------------------
 @variable(m1,  F[1: N_hour])
 @variable(m1,  O[1: N_hour])
 @variable(m1,  l[1: N_hour ])
@@ -61,43 +62,45 @@ m1 = Model(Ipopt.Optimizer)
 @variable(m1,  P[1:N_hour, 1:N_ISO])
 @variable(m1,  E[1:N_hour, 1:(N_ISO+1)])
 
-#Bounds and Starting Points----------------------------------
+##Bounds and Starting Points----------------------------------
 for k in 1:N_hour, s in 1:(N_ISO+1)
-    #Bounds
+      #Bounds
       set_lower_bound(F[k], 0)
       set_upper_bound(F[k], 5)
 
 
       set_lower_bound(O[k], 0)
       set_upper_bound(O[k], 5)
-  
 
-    set_lower_bound(l[k], 0)
-    set_upper_bound(l[k], 5)
 
-    set_lower_bound(E[k, s], Eₘᵢₙ)
-    set_upper_bound(E[k, s], Eₘₐₓ)
-    #Start Values
-    
-set_start_value(F[k], F0)
-set_start_value(O[k], O0)
+      set_lower_bound(l[k], 0)
+      set_upper_bound(l[k], 5)
 
-    set_start_value(l[k], l0)
-    set_start_value(E[k, s], E0)
+      set_lower_bound(E[k, s], Eₘᵢₙ)
+      set_upper_bound(E[k, s], Eₘₐₓ)
+      #Start Values
+
+      set_start_value(F[k], F0)
+      set_start_value(O[k], O0)
+      set_start_value(l[k], l0)
+      set_start_value(E[k, s], E0)
 end
 
 for k in 1:N_hour, s in 1:(N_ISO)
-
-set_lower_bound(P[k, s], -5)
-set_upper_bound(P[k, s], 5)
-
-set_start_value(P[k, s], P0)
-
+      set_lower_bound(P[k, s], -5)
+      set_upper_bound(P[k, s], 5)
+      set_start_value(P[k, s], P0)
 end
+
 ##Constraints----------------------------------
 
+#Initialization of differential state(s)
 @constraints(m1, begin
       Initial_State, E[1, 1] == E0
+end)
+#Continuity
+@constraints(m1, begin
+      Connecting[k in 1:N_hour-1], E[k+1, 1] == E[k, end]
 end)
 
 #C1
@@ -105,31 +108,24 @@ end)
       Battery_Power[k in 1:N_hour, s in 1:N_ISO], P[k, s] == α[k, s] * F[k] + O[k] - l[k]
 end)
 
-
 #C2
 @constraints(m1, begin
       Battery_Energy[k in 1:N_hour, s in 1:N_ISO], E[k, s+1] == E[k, s] + P[k,s] * dt/3600
 end)
 
-@constraints(m1, begin
-      Connecting[k in 1:N_hour-1], E[k+1, 1] == E[k, end]
-end)
-
-
-
 #C3
 @constraints(m1, begin
       Battery_Safety[k in 1:N_hour, s in 1:(N_ISO+1)], Eₘᵢₙ + τₗ*ΔE <= E[k, s] <= Eₘₐₓ - τᵤ*ΔE
 end)
-#C4
 
+#C4
 @constraints(m1, begin
       Battery_Terminate[k in 1:N_hour], Eₘᵢₙ + μₗ*ΔE <= E[end, end] <= Eₘₐₓ - μᵤ*ΔE
 end)
 
 
 #Objective Function----------------------------------
-@objective(m1, Max,  sum((Πᶠ[k] * F[k]) - (Πᵉ[k] * O[k]) for k in 1:N_hour))
+@objective(m1, Min,  sum( (Πᵉ[k] * O[k]) - (Πᶠ[k] * F[k]) for k in 1:N_hour))
 ##Optimization!----------------------------------
 optimize!(m1)
 JuMP.termination_status(m1)
@@ -138,8 +134,20 @@ JuMP.solve_time(m1)
 
 ##Extracting Data----------------------------------
 
-plotly()
-t_plot = collect(range(0, stop = N_hour, length = 3600*N_hour ) )       #!FIX STEPS CORRECTLY
+plotlyjs()
+#Creating time axis
+#t_plot = collect(range(0, stop = N_hour, length = N_ISO*N_hour ) ) #!FIX STEPS CORRECTLY
+t_plot = Float64[];
+for i in 1:N_hour
+      global t_plot
+      if i != N_hour
+            t_sample = collect(range(i-1, stop = i, length = N_ISO+1 ) );
+            pop!(t_sample);
+      else
+            t_sample = collect(range(i-1, stop = i, length = N_ISO ) );
+      end
+      t_plot = cat(t_plot, t_sample, dims = 1)
+end
 
 
 fr = JuMP.value.(F)[:]
@@ -147,30 +155,38 @@ Ok = JuMP.value.(O)[:]
 Ek = JuMP.value.(E)[:,1:end-1]
 Pk = JuMP.value.(P)[:,:]
 
-Ek = reshape( transpose(Ek), (1,:))
-Pk = reshape( transpose(Pk), (1,:))
+Ek = reshape(transpose(Ek), (1,:))
+Pk = reshape(transpose(Pk), (1,:))
 
-profit = Πᶠ.*fr .- Πᵉ.*Ok
+profit = Πᶠ.*fr .- Πᵉ.*Ok;
 
 
 p11 = plot(t_plot, Ek[1,:], label = "E")
 p11 = plot!(t_plot, Pk[1,:], label = "P")
+p11 = plot!( xlims=(0,N_hour))
+t_plotₖ = cat(t_plot[N_ISO+1:N_ISO:end], t_plot[end], dims = 1);
 
-p12 = plot(t_plot[1:3600:end],  fr, linetype = :steppre, label = "F")
-p12 = plot!(t_plot[1:3600:end], Ok, linetype = :steppre, label = "O")
 
-p13 = plot(t_plot[1:3600:end], Πᶠ, ls = :dash, linetype = :steppre, label = "pi_f")
-p13 = plot!(t_plot[1:3600:end], Πᵉ, ls = :dash, linetype = :steppre, label = "pi_e")
-p13 = plot!(t_plot[1:3600:end], profit, linetype = :steppre , label = "profit")
 
-p2 = plot(p12, p13, layout = (2, 1))
 
-display(p11)
-display(p2)
+p12 = plot(  t_plotₖ, fr, ls = :dot,  markershape = :circle, mc = :match,label = "F")
+p12 = plot!( t_plotₖ, Ok, ls = :dot, markershape = :circle, mc = :match,label = "O")
+p12 = plot!( xlims=(0,N_hour))
+
+p13 = plot(  t_plotₖ, Πᶠ, ls = :dot,  markershape = :hexagon, label = "Πᶠ")
+p13 = plot!( t_plotₖ, Πᵉ, ls = :dot,  markershape = :hexagon,label = "Πᵉ")
+p13 = plot!( t_plotₖ, profit, ls = :dot,  markershape = :hexagon, mc = :match,label = "Profit")
+p13 = plot!( xlims=(0,N_hour))
+
+p2 = plot(p12, p13, p11, layout = (3, 1), legend=:topleft)
+
+#display(p11)
+#display(p2)
 
 ##
+savefig(p2,"~/Documents/Julia/1.Thesis/Battery_FP/Figures/OCP_Result.eps")
 
-
+#savefig(p11,"~/Documents/Julia/1.Thesis/Battery_FP/Figures/OCP_Result1.eps")
 
 # FR  =   cat(F0, fr[1:N_hour], dims = 1)
 # DAM_P = cat(O0, Ok[1:N_hour], dims = 1)
@@ -193,3 +209,7 @@ display(p2)
 # p3 = plot(t_plot, E_Plot, label = "E", linetype = :steppost, linestyle = :dash);
 
 # fig1 = plot(p1, p2, p3, layout = (3, 1), xaxis = "Time")
+
+
+
+
